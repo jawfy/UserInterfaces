ad42cf9e71b0a309eeb8d6d3c05766b6f3e49256
Jack Earsman <me@jackearsman.com>
2017-05-17 11:32:57 +0100

    Commit

---
+++ js/button-change.js
@@ -0,0 +1,36 @@
+/* Change Code */
+var c, w;
+c = "Awesome! You changed it.";
+w = "Websites";
+
+function Change() {
+    document.getElementById("change").innerHTML = c;
+}
+
+function Websites() {
+    document.getElementById("websites").innerHTML = w;
+}
+/* Change Code END */
+
+/* Subscription Code */
+var t;
+t = "Awesome! You subscribed.";
+
+function Sub() {
+    document.getElementById("sub").innerHTML = t;
+}
+
+/* Subscription Code END */
+
+
+/* Junk I may use */
+function Location() {
+    location.href = "thanks.html";
+}
+
+function Thanks() {
+    document.getElementsById("sub").innerHTML = "Thanks for subscribing";
+}
+
+var loc;
+thnx = "Thanks for subscribing!";
